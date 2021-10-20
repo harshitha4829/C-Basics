39eac829e1024c7fcdf5043e7be9f461c42e0df1
harshitha4829 <92290996+harshitha4829@users.noreply.github.com>
2021-10-20 12:40:09 +0530

    Add files via upload

---
+++ data types.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	//Initialization
+	age = 19;
+	cgpa = 8.4;
+	initial ='B';
+	//output
+	printf("%d\n",age);
+	printf("%f\n",cgpa);
+	printf("%c\n",initial);
+	
+}
